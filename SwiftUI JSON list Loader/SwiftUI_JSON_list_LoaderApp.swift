d475abf964cd9e9c3127634ea3142b5a2061c379
Juan Hernandez <juanhernandez98@gmail.com>
2021-05-01 10:47:23 -0700

    Initial Commit

---
+++ SwiftUI JSON list Loader/SwiftUI_JSON_list_LoaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_JSON_list_LoaderApp.swift
+//  SwiftUI JSON list Loader
+//
+//  Created by Juan Hernandez on 5/1/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_JSON_list_LoaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
